8b83f638165a1f75035854ef3a4806389471eda1
Ashkan Amani <amaniashkan2000@gmail.com>
2023-02-05 12:11:18 +0330

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/ashkanamani/booking/pkg/config"
+	"github.com/ashkanamani/booking/pkg/handlers"
+	"github.com/ashkanamani/booking/pkg/render"
+)
+
+const portNumber = ":8000"
+
+var app config.AppConfig
+
+var session *scs.SessionManager
+// main is the main applicatoin function
+func main() {
+
+
+	// change it to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	
+	app.Session = session
+	tc, err := render.CeateTemplateCache()
+	if err != nil {
+		log.Fatal("Can not create template cache!", err)
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := http.Server{
+		Handler: routes(&app),
+		Addr: portNumber,
+	}
+	err = srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ pkg/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"html/template"
+	"log"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+// AppConfig holds the application config
+type AppConfig struct {
+	TemplateCache map[string]*template.Template
+	UseCache      bool
+	InfoLog       *log.Logger
+	ErrorLog      *log.Logger
+	InProduction  bool
+	Session       *scs.SessionManager
+}
